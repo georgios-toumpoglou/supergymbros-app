32e0ff60beddc51973349ba5a362d17d15d94fbf
George <gtoumb@gmail.com>
2026-05-08 20:56:44 +0300

    Fix guicorn version

--- main.py
+++ main.py
@@ -620,5 +620,8 @@ def next_week_set():
     return redirect(url_for('schedule') + '?view=next')
 
 
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
